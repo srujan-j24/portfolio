43e41d32dd23d25261967a11ed319cdcd311c2d3
srujan-j24 <srujanj2021@gmail.com>
2024-03-30 20:52:20 +0530

    may be final update on navbar

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-import "./style.scss"
+import "./style.scss";
+
+let root = document.documentElement;
 import { navBarInit } from "./nav-bar";
 
-navBarInit();
+const store = {
+    windowWidth: window.innerWidth,
+    root: document.documentElement,
+}
+window.addEventListener('resize', ()=>{
+    store.windowWidth = window.innerWidth;
+});
+
+function followMouse(e, fn){
+    root.style.setProperty('--mouse-x', e.clientX);
+    root.style.setProperty('--mouse-y', e.clientY);
+    fn(e.clientX);
+}
+function stopFollowMouse(){
+    document.removeEventListener('mousemove', followMouse);
+}
+
+
+navBarInit(store).then((fn)=>{
+    document.addEventListener('mousemove', (e)=>{followMouse(e, fn)});
+});
+
 
--- nav-bar.js
+++ nav-bar.js
@@ -1,82 +1,83 @@
-export function navBarInit(){
-    let wrapper = document.getElementById("h-line-wrapper");
-    let navbarStyle = document.getElementById('navbar').style;
+export function navBarInit(store){
+    let navBar = document.getElementById("navbar");
+    let lineCont = document.getElementById('line-cont');
+    let lineContSytle = lineCont.style;
     let navElements = Array.from(document.getElementsByClassName('nav-li'));
-    let vGrdMouse = '--grd-mouse';
-    let vGrdStart = '--grd-start';
-    let vGrdMid = '--grd-mid';
-    let vGrdEnd = '--grd-end';
-    let vGrdColor = '--grd-color';
-    let grdMidPos = 50;
-    let grdStartPos = 50;
-    let grdEndPos = 50;
 
-    let grdCornerInterval;
-    let mainOffset = 50;
-    console.log(navElements)
-
-    function setNavVar(name, value){
-        navbarStyle.setProperty(name, value);
-    }
-
-    function fixGrdCorners(startPos, endPos, delay){
-        let curGrdStyle = getComputedStyle(wrapper, '::after');
-        grdStartPos = parseInt(curGrdStyle.getPropertyValue("height").slice(0, -1));
-        grdEndPos =  parseInt(curGrdStyle.getPropertyValue("width").slice(0, -1));
+    let curOffset = 0;
+    let cornersFixed = {val: true};
+    let fixGrdCornersInterval; 
+    function fixGrdCorners(startPos, endPos, pixelSkip){
+        cornersFixed.val = true;
+        let computedLineStyle = window.getComputedStyle(lineCont);
+        let curStartPos = parseInt(computedLineStyle.getPropertyValue('--grd-start'));
+        let curEndPos = parseInt(computedLineStyle.getPropertyValue('--grd-end'));
         startPos = parseInt(startPos);
         endPos = parseInt(endPos);
-        clearInterval(grdCornerInterval);
+        clearInterval(fixGrdCornersInterval);
         return new Promise((resolve)=>{
-            let startDir = grdStartPos < startPos ? +1 : -1;
-            let endDir = grdEndPos > endPos ? -1 : +1;
-
-            grdCornerInterval = setInterval(()=>{
-                if(grdStartPos != startPos){
-                    setNavVar(vGrdStart, `${grdStartPos+=startDir}%`);
+            let startDir = startPos > curStartPos ? +1 : -1;
+            let endDir = endPos < curEndPos ? -1 : +1;
+            fixGrdCornersInterval = setInterval(()=>{
+                if(startPos != curEndPos){
+                    curStartPos = Math.abs(curStartPos - startPos) < pixelSkip ?  startPos : curStartPos + startDir * pixelSkip;
+                    lineContSytle.setProperty('--grd-start', curStartPos);
                 }
-                if(grdEndPos != endPos){
-                    setNavVar(vGrdEnd, `${grdEndPos+=endDir}%`);
+                if(endPos != curEndPos){
+                    curEndPos = Math.abs(curEndPos - endPos) < pixelSkip ? endPos : curEndPos + endDir * pixelSkip;
+                    lineContSytle.setProperty('--grd-end', curEndPos);
                 }
-                if(grdStartPos == startPos && grdEndPos == endPos){
-                    clearInterval(grdCornerInterval);
+                if(startPos == curStartPos && endPos == curEndPos){
+                    clearInterval(fixGrdCornersInterval);
                     resolve();
                 }
-            }, delay);
+            }, 0);
         })
     }
-
-    function addhoverEffect(){
-        let expandTimeOut;
+    let expandTimeOut;
+    function addHoverEffect(){
         for(let element of navElements){
-            element.addEventListener("mouseover", ()=>{
-                clearTimeout(expandTimeOut);
-                let {right, left} = element.getBoundingClientRect();
-                let ww = window.innerWidth;
-                fixGrdCorners(Math.floor(left/ww*100), Math.ceil(right/ww*100), 5);
-            });
-            element.addEventListener("mouseout", ()=>{
-                let {width} = element.getBoundingClientRect();
-                let offset = width / window.innerWidth * 100 / 2;
-                setNavVar(vGrdStart, `calc(var(${vGrdMouse}) - ${offset}%)`);
-                setNavVar(vGrdEnd, `calc(var(${vGrdMouse}) + ${offset}%)`);
-                mainOffset = offset;
+            element.addEventListener('mouseover', ()=>{
+                clearTimeout(expandTimeOut)
+                lineCont.classList.remove('move-with-mouse');
+                let elementRect = element.getBoundingClientRect();
+                fixGrdCorners(elementRect.left, elementRect.right, 7);
+                curOffset = parseInt(elementRect.width/2); 
+                lineContSytle.setProperty('--grd-end-offset', `${curOffset}`);
+                lineContSytle.setProperty('--grd-start-offset', `${curOffset}`);
+            })
+            element.addEventListener('mouseout', ()=>{
+                lineCont.classList.add('move-with-mouse');
+                cornersFixed.val = false;
                 expandTimeOut = setTimeout(()=>{
-                    fixGrdCorners(0, 100, 10)
-                }, 500)
+                    lineCont.classList.remove('move-with-mouse');
+                    fixGrdCorners(0, store.windowWidth, 5)
+                }, 1000);
             })
         }
-    }
-    function followMouse(){
-        document.addEventListener("mousemove", (e)=>{
-            let curMousePercent = e.clientX / window.innerWidth * 100;
-            setNavVar(vGrdMouse, `${curMousePercent}%`);
-        })
+        
     }
 
+    function updateWhileFollow(mouseX){
+        console.log(cornersFixed)
+        if(!cornersFixed.val){
+            lineContSytle.setProperty('--grd-start', parseInt(mouseX - curOffset));
+            lineContSytle.setProperty('--grd-end', parseInt(mouseX + curOffset));
+        }
+    }
+    return new Promise((resolve)=>{
+        store.root.style.setProperty('--mouse-x', store.windowWidth/2)
+        lineContSytle.setProperty('--grd-start', store.windowWidth/2);
+        lineContSytle.setProperty('--grd-end', store.windowWidth/2);
 
-    fixGrdCorners(0, 100, 15)
-    .then(()=>{
-        followMouse();
-        addhoverEffect();
-    })
+        fixGrdCorners(0, store.windowWidth, 3)
+            .then(()=>{
+                addHoverEffect();
+                resolve(updateWhileFollow);
+            });
+    });
+
+
+
+    
 }
